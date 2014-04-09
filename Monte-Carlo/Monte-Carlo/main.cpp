8c731a278e19a7cce18fb18dd730584e380a5c50
ubuntu <ubuntu@ubuntu-VirtualBox.(none)>
2014-04-09 10:48:30 +0200

    add  gnuplot script and generation of data in main.cpp

--- Monte-Carlo/Monte-Carlo/main.cpp
+++ Monte-Carlo/Monte-Carlo/main.cpp
@@ -4,15 +4,16 @@
 #include "Asset.h"
 #include "MonteCarloCdoEngine.h"
 #include "var_alea.h"
+#include <iostream>
+#include <fstream> 
 
 using namespace std;
 
 
 
-const Cdo BuildCdo()
+const Cdo BuildCdo(double k1, double k2)
 {
 	int nbAssets = 125;
-	double K1 = 0.2, K2 = 0.3;
 	double lambda = 0.01;//default intensity
 	double recoveryRate = 0.4;
 	std::vector<double> spreadPaimentDates{ 0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5 };
@@ -29,28 +30,42 @@ const Cdo BuildCdo()
 		assets.push_back(asset);
 	}
 
-	return Cdo(K1, K2, spreadPaimentDates, assets);
+	return Cdo(k1, k2, spreadPaimentDates, assets);
 }
 
 
 int main()
 {
-
+	int nbSimu = 1000;
 	init_alea();
-	auto myCdo = BuildCdo();
 	double rate = 0.01;
 
 	MonteCarloCdoEngine engine;
 
-	auto result = engine.Price(myCdo, 1000, rate);
 
-	cout << "spread: " << result.Spread << endl;
-	char pause;
-	cin >> pause;
+	std::vector<double> k1Vect;
+	std::vector<double> spreadVect;
+	for (int i = 0; i < 10; i++)
+	{
+		double k1 = i/(double)10;
+		double k2 = (i+1)/(double)10;
+		const Cdo cdo = BuildCdo(k1,k2);
+		auto result = engine.Price(cdo, nbSimu, rate);
+		double spread = result.Spread;
+		k1Vect.push_back(k1);
+		spreadVect.push_back(spread);
+	}
+
+
+ 	std::ofstream outputFile;
+  	outputFile.open ("test.data");
+  	outputFile << "#k1 spread dummy" << endl;
+  	for(int i = 0; i < k1Vect.size(); i++)
+  	{
+  		outputFile << k1Vect[i] << " " << spreadVect[i] << " " << cos(i) << endl;
+  	}
+  	outputFile.close();
+
 	return 0;
 }
 
-
-
-//TODO
-//-Recovery rate
---
+++ Monte-Carlo/Monte-Carlo/script.gnu
@@ -0,0 +1,2 @@
+set title "Demo of plotting financial data" 
+plot 'test.data' using 1:2 notitle with lines, '' using 1:3 notitle with lines
